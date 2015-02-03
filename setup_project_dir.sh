b96a534e63f2c8a0f0b63890a92acd1ca96e197f
Daniel Chen <Maveri2k@yahoo.com>
2015-02-03 16:45:07 -0500

    initial commit and script

---
+++ setup_project_dir.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# the first argument passed into the script should be the dir
+# where you want the folder structure setup
+
+echo "setting up folder structure in $1"
+
+cd $1
+mkdir doc data src bin results
+
+cd doc
+echo "Doc directory with one subdirectory per manuscript" > README
+
+cd ../data
+echo "Data directory for storing fixed data sets" > README
+
+cd ../src
+echo "src for source code" > README
+
+cd ../bin
+echo "bin for compiled binaries or scripts" > README
+
+cd ../results
+echo "Results directory for tracking computational experiments peformed on data" > README
+
+echo "Folders created."
